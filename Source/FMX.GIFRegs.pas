67265bde68d7d07768a471e0369c83c5ccd5c875
Thomas Dannert <35112226+TomDannert@users.noreply.github.com>
2020-06-15 20:25:31 +0200

    Update FMX.GIFRegs.pas

--- Source/FMX.GIFRegs.pas
+++ Source/FMX.GIFRegs.pas
@@ -11,7 +11,7 @@
   FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
   for more details.
   **************************************************************************** }
-
+
 unit FMX.GIFRegs;
 
 interface
